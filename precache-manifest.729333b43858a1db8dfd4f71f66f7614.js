f32673ccd9b8aad9ff43dd4fba6a4e1feb6b2235
cuong <cuongvoong@gmail.com>
2018-10-26 20:01:55 -0700

    Updates

---
+++ precache-manifest.729333b43858a1db8dfd4f71f66f7614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed0691ef61640e310613",
+    "url": "/react-classic-tetris/static/js/runtime~main.ed0691ef.js"
+  },
+  {
+    "revision": "9d6a09de2f5f5f8b0b96",
+    "url": "/react-classic-tetris/static/js/main.9d6a09de.chunk.js"
+  },
+  {
+    "revision": "d87350e4a65c5c04de9b",
+    "url": "/react-classic-tetris/static/js/1.d87350e4.chunk.js"
+  },
+  {
+    "revision": "9d6a09de2f5f5f8b0b96",
+    "url": "/react-classic-tetris/static/css/main.2d98dcdf.chunk.css"
+  },
+  {
+    "revision": "d87350e4a65c5c04de9b",
+    "url": "/react-classic-tetris/static/css/1.1aa00f11.chunk.css"
+  },
+  {
+    "revision": "4cd0d31116bdf908a5e241b9a7c86a49",
+    "url": "/react-classic-tetris/index.html"
+  }
+];
